014fa19f2d5063d0657b21d31213bc92961d6025
suracefm <suracefm@github.com>
2019-07-03 09:53:54 +0200

    Added mybinder env

---
+++ pyqm/__init__.py
@@ -0,0 +1,1 @@
+from pyqm.pyqm import *
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Setup file created
+"""
+from setuptools import setup
+
+setup(name='pyqm',
+      version='0.1',
+      description='',
+      url='https://github.com/suracefm/pyqm',
+      author='Federica Maria Surace',
+      license='BSD 3 New',
+      packages = ['pyqm']
+)
